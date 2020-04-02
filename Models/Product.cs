80a13a3089ea65473baecaa4b7273cccd8879647
varun-vp <varun.pahuja96@gmail.com>
2020-04-02 15:35:16 +0530

    initial commit

---
+++ Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GitOurProductExample.Models
+{
+    public class Product
+    {
+        public int PId{ get; set; }
+        public string PName { get; set; }
+        public int PDesc { get; set; }
+        public int PPrice { get; set; }
+
+
+    }
+}
