22c8db66e6ef6eab66aa8eaf301e7787f69d2ff1
varun-vp <varun.pahuja96@gmail.com>
2020-04-02 15:54:54 +0530

    third commit with migration and update-datbase command

---
+++ Migrations/20200402102333_first-mig.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GitOurProductExample.Migrations
+{
+    public partial class firstmig : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    PId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PName = table.Column<string>(nullable: true),
+                    PDesc = table.Column<int>(nullable: false),
+                    PPrice = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.PId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+        }
+    }
+}
