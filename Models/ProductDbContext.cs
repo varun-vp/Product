a9de61192bc1bf62b8635b89dd835744e66bc42e
varun-vp <varun.pahuja96@gmail.com>
2020-04-02 15:52:45 +0530

    final commit with controller

--- Models/ProductDbContext.cs
+++ Models/ProductDbContext.cs
@@ -1,11 +1,17 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace GitOurProductExample.Models
 {
-    public class ProductDbContext
+    public class ProductDbContext : DbContext
+
     {
+        public ProductDbContext( DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Product> Products { get; set; }
     }
 }
